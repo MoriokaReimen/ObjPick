8689ea0280bea005973e6f24921d17fbfe30a63b
MoriokaReimen <kei.nakata.job@gmail.com>
2023-01-07 11:57:06 +0900

    [update] add module system

---
+++ include/Render/IModule.hpp
@@ -0,0 +1,35 @@
+#ifndef IMODULE_HPP
+#define IMODULE_HPP
+/*
+  ███╗░░░███╗░█████╗░██████╗░██╗░█████╗░██╗░░██╗░█████╗░
+  ████╗░████║██╔══██╗██╔══██╗██║██╔══██╗██║░██╔╝██╔══██╗
+  ██╔████╔██║██║░░██║██████╔╝██║██║░░██║█████═╝░███████║
+  ██║╚██╔╝██║██║░░██║██╔══██╗██║██║░░██║██╔═██╗░██╔══██║
+  ██║░╚═╝░██║╚█████╔╝██║░░██║██║╚█████╔╝██║░╚██╗██║░░██║
+  ╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+  2023/1/7 
+*/
+#include <entt/entt.hpp>
+
+namespace Render
+{
+  class IModule
+  {
+    protected:
+      entt::registry& registry_;
+    public:
+
+      IModule(entt::registry& registry)
+        : registry_(registry)
+      {
+      }
+
+      virtual ~IModule()
+      {
+      }
+
+      virtual void init() = 0;
+      virtual void update() = 0;
+  };
+}
+#endif
---
+++ include/Render/Window.hpp
@@ -0,0 +1,30 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+/*
+  ███╗░░░███╗░█████╗░██████╗░██╗░█████╗░██╗░░██╗░█████╗░
+  ████╗░████║██╔══██╗██╔══██╗██║██╔══██╗██║░██╔╝██╔══██╗
+  ██╔████╔██║██║░░██║██████╔╝██║██║░░██║█████═╝░███████║
+  ██║╚██╔╝██║██║░░██║██╔══██╗██║██║░░██║██╔═██╗░██╔══██║
+  ██║░╚═╝░██║╚█████╔╝██║░░██║██║╚█████╔╝██║░╚██╗██║░░██║
+  ╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝
+  2023/1/7 
+*/
+#include "Render/IModule.hpp"
+#include <entt/entt.hpp>
+
+namespace Render
+{
+  class Window :IModule
+  {
+    public:
+      struct Context
+      {
+      };
+    public:
+      Window(entt::registry& registry);
+      ~Window() override;
+      void init() override;
+      void update() override;
+  };
+}
+#endif
